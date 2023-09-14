ded9661c42344d0064042b80fbc6899313694d5a
student-ablack12 <91693765+student-ablack12@users.noreply.github.com>
2023-09-14 10:06:18 -0400

    test build, trying to add template to this file. pushing first to show on desktop

---
+++ MadLibs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MadLibs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader fileReader = new StreamReader("");
+        }
+    }
+}
