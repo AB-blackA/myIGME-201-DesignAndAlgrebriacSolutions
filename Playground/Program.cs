fcded4c30c6056cadd68afc11012aece2a1e1a73
student-ablack12 <91693765+student-ablack12@users.noreply.github.com>
2023-09-22 09:03:36 -0400

    intial commit, it was a bit confusing at first but we got there

---
+++ Playground/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int var1 = 11;
+            int var2 = 1;
+
+            bool b = (var1 > 10) ^ (var2 > 10);
+
+            Console.WriteLine(b);
+        }
+    }
+}
