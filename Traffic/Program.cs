3e25a08d7f84647b6752e9288904ee3f63eccce4
Andrew Black <acb7086@rit.edu>
2023-10-10 10:45:31 -0400

    initial commit, test features working

---
+++ Traffic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles;
+
+namespace Traffic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Compact co = new Compact();
+
+            FreightTrain ft = new FreightTrain();
+
+            AddPassenger(co);
+            //AddPassenger(ft);
+
+        }
+
+        static void AddPassenger(PassengerCarrier pc)
+        {
+            Console.WriteLine("I was called");
+            pc.LoadPassenger();
+            Console.WriteLine(pc.ToString());
+        }
+    }
+}
