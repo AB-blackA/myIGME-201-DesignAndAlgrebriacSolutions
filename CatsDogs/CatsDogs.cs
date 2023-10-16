1c04e4f07beed6bbb3c3ff7fc129f942587da668
student-ablack12 <91693765+student-ablack12@users.noreply.github.com>
2023-10-16 13:01:47 -0400

    initial commit of CatsDogs library and main program that utilizes it, working well. only issue is not understanding how to use TypeOf effectively for assigning interfaces. Try/Catch used instead...

---
+++ CatsDogs/CatsDogs.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatsDogs
+{
+    public interface ICat
+    {
+
+        void Eat();
+
+        void Play();
+
+        void Scratch();
+
+        void Purr();
+
+    }
+
+    public interface IDog
+    {
+        void Eat();
+
+        void Play();
+
+        void Bark();
+
+        void NeedWalk();
+
+        void GotoVet();
+
+    }
+
+    public abstract class Pet
+    {
+
+        private string name;
+
+        public int age;
+
+        public Pet()
+        {
+
+        }
+
+        public Pet(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public abstract void Eat();
+        public abstract void Play();
+        public abstract void GotoVet();
+
+        
+    }
+
+    public class Cat : Pet, ICat
+    {
+        public Cat()
+        {
+
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("{0}: I cannot wait for you to perish so I can eat something GOOD for once...", base.Name);
+        }
+
+        public override void GotoVet()
+        {
+            Console.WriteLine("{0}: Time to dissapear!!! *scrams*", base.Name);
+        }
+
+        public override void Play()
+        {
+            Console.WriteLine("{0}: For what reason do you interrupt me, huma- IS THAT A FEATHER ON A STRING? *paws wildly*", base.Name);
+        }
+
+        public void Purr()
+        {
+            Console.WriteLine("{0}: Prrrrrrrrrrrrrhh...", base.Name);
+        }
+
+        public void Scratch()
+        {
+            Console.WriteLine("{0}: *HISSSSSSSSSSS*", base.Name);
+        }
+    }
+
+    public class Dog : Pet, IDog
+    {
+
+        public string license;
+
+        public Dog(string license, string name, int age)
+        {
+            this.license = license;
+            base.Name = name;
+            base.age = age;
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("{0}: WOOF WOOF *WAGS TAIL*", base.Name);
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("{0}: I WOULD EAT ANYTHING FROM YOU, BEST FRIEND FOREVER", base.Name);
+        }
+
+        public override void GotoVet()
+        {
+            Console.WriteLine("{0}: WHY HAVE YOU FORSAKEN ME *whimpers*", base.Name);
+        }
+
+        public void NeedWalk()
+        {
+            Console.WriteLine("{0}: IF I SCRATCH THE DOOR ENOUGH IT WILL OPEN", base.Name);
+        }
+
+        public override void Play()
+        {
+            Console.WriteLine("{0}: GET BALL RETURN BALL GET BALL RETURN BALL BALL BALL BALL", base.Name);
+        }
+    }
+
+    public class Pets
+    {
+        List<Pet> petList = new List<Pet>();
+
+        public Pet this[int nPetEl]
+        {
+            get
+            {
+                Pet returnVal;
+                try
+                {
+                    returnVal = (Pet)petList[nPetEl];
+                }
+                catch
+                {
+                    returnVal = null;
+                }
+
+                return (returnVal);
+            }
+
+            set
+            {
+                // if the index is less than the number of list elements
+                if (nPetEl < petList.Count)
+                {
+                    // update the existing value at that index
+                    petList[nPetEl] = value;
+                }
+                else
+                {
+                    // add the value to the list
+                    petList.Add(value);
+                }
+            }
+        }
+
+        public  int Count
+        {
+            get { return this.petList.Count; }
+        }
+
+        public void Add(Pet pet)
+        {
+            this.petList.Add(pet);
+        }
+
+        public void Remove(Pet pet)
+        {
+            this.petList.Remove(pet);
+        }
+
+        public void RemoveAt(int petEI)
+        {
+            this.petList.RemoveAt(petEI);
+        }
+    }
+}
---
+++ PE13 - CatsDogs/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatsDogs;
+
+namespace PE13___CatsDogs
+{
+    /* Author: Andrew Black since 10/16/23
+     * Purpose: Program makes use of Cat and Dog classes for practice
+     * Limitations: Requires use of CatsDogs library
+     */
+    internal class Program
+    {
+        /* Method: Main
+         * Purpose: Create Pets and use some of their functions
+         * Limitations: none
+         */
+        static void Main(string[] args)
+        {
+            Pet thisPet = null;
+            Dog dog = null;
+            Cat cat = null;
+            IDog iDog = null;
+            ICat iCat = null;
+
+            Pets pets = new Pets();
+
+            Random rand = new Random();
+
+            for(int i = 0; i < 50; i++)
+            {
+                //if else provided by professor
+                // 1 in 10 chance of adding an animal
+                if (rand.Next(1, 11) == 1)
+                {
+
+                    string name;
+                    int age;
+                    string license;
+
+                    //add dog
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        Console.WriteLine("Congratulations on adopting a dog!\n" +
+                            "What's their name?");
+                        name = Console.ReadLine();
+
+                    askAge:
+                        Console.WriteLine("What's their age?");
+                        try
+                        {
+                            age = Int16.Parse(Console.ReadLine());
+                        }
+                        catch (Exception e)
+                        {
+                            goto askAge;
+                        }
+
+                        Console.WriteLine("What's their license?");
+                        license = Console.ReadLine();
+
+                        pets.Add(new Dog(license, name, age));
+
+                    }
+
+                    // else add a cat
+                    else
+                    {
+                        Console.WriteLine("Congratulations on adopting a cat!\n" +
+                            "What's their name?");
+                        name = Console.ReadLine();
+
+                    askAge:
+                        Console.WriteLine("What's their age?");
+                        try
+                        {
+                            age = Int16.Parse(Console.ReadLine());
+                        }
+                        catch (Exception e)
+                        {
+                            goto askAge;
+                        }
+
+                        cat = new Cat
+                        {
+                            Name = name,
+                            age = age
+                        };
+
+                        pets.Add(cat);
+                        
+
+                    }
+                }
+                else
+                {
+                    // choose a random pet from pets and choose a random activity for the pet to do
+
+                    thisPet = pets[rand.Next(0, pets.Count)];
+
+                    //number of random activities is five, used in switch case
+                    int activity = rand.Next(0, 5);
+
+                    if (thisPet == null)
+                    {
+                        continue;
+                    }
+
+                    //try for each interface possibility
+                    try { 
+                        iCat = (ICat)thisPet;
+                        switch (activity)
+                        {
+                            //pick one of five activities
+                            case 0:
+                                iCat.Eat();
+                                break;
+                            case 1:
+                                iCat.Play();
+                                break;
+                            case 2:
+                                thisPet.GotoVet();
+                                break;
+                            case 3:
+                                iCat.Purr();
+                                break;
+                            case 4:
+                                iCat.Scratch(); 
+                                break;
+                        }
+                    }
+
+                    catch 
+                    { 
+                        iDog = (IDog)thisPet;
+                        switch (activity)
+                        {
+                            //pick one of five activities
+                            case 0:
+                                iDog.Eat();
+                                break;
+                            case 1:
+                                iDog.Play();
+                                break;
+                            case 2:
+                                thisPet.GotoVet();
+                                break;
+                            case 3:
+                                iDog.Bark();
+                                break;
+                            case 4:
+                                iDog.NeedWalk();
+                                break;
+                        }
+                    }
+
+
+                }
+
+            }
+        }
+    }
+}
