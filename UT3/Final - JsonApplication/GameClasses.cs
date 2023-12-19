6df55e2593352b6d4378a6a7a3155cded51a1945
Andrew Black <acb7086@rit.edu>
2023-12-18 21:31:11 -0500

    created project and added meaningful comments

---
+++ UT3/Final - JsonApplication/GameClasses.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+/* Author: Andrew Black, since 12/18/23
+ * Purpose: Classes that uses JSON deserializing classes to load and save a player's data for a theoretically RPG.
+ * Note that this is only for the purpose of using JSON and is not a real game. Per specs of IGME201 final.
+ */
+namespace Final___JsonApplication
+{
+
+    /* Class: Player
+     * Purpose: class that contains player information
+     */
+    public class Player
+    {
+        public string PlayerName { get; set; }
+        public int Level { get; set; }
+        public int HP { get; set; }
+        public string[] Inventory { get; set; }
+        public string LicenseKey { get; set; }
+    }
+
+    /* Class: SaveManager
+     * Purpose: Allows loading and saving of Json file for a Player.
+     */
+    public class SaveManager
+    {
+        private static SaveManager instance;
+        private static readonly object lockObject = new object();
+
+        private SaveManager() { }
+
+        public static SaveManager Instance
+        {
+            get
+            {
+                lock (lockObject)
+                {
+                    if (instance == null)
+                    {
+                        instance = new SaveManager();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        /* Method: SaveSettings
+         * Purpose: saves the Player info to the json file
+         */
+        public void SaveSettings(Player settings, string filePath)
+        {
+            string json = JsonConvert.SerializeObject(settings);
+            File.WriteAllText(filePath, json);
+        }
+
+        /* Method: LoadSettings
+         * Purpose: Loads the Player information from a json file
+         */
+        public Player LoadSettings(string filePath)
+        {
+            //provided there is a file, load it into a Player and return
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<Player>(json);
+            }
+            else
+            {
+                //if the file doesn't exist, creates a new Player ("new game")
+                return new Player();
+            }
+        }
+    }
+}
---
+++ UT3/Final - JsonApplication/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+/* Author: Andrew Black, since 12/18/23
+ * Purpose: Program that uses JSON deserializing classes to load and save a player's data for a theoretically RPG.
+ * Note that this is only for the purpose of using JSON and is not a real game. Per specs of IGME201 final.
+ * Limitations: requires reference to GameClasses.cs
+ */
+namespace Final___JsonApplication
+{
+
+    
+    class Program
+    {
+        /* Method: Main
+         * Purpose: test features of Class's Player and SaveManager
+         */
+        static void Main()
+        {
+            //test code added in to create initial json file from speculations given
+            /*Player playerSettings = new Player
+            {
+                PlayerName = "dschuh",
+                Level = 4,
+                HP = 99,
+                Inventory = new[]
+           {
+                "spear", "water bottle", "hammer", "sonic screwdriver", "cannonball",
+                "wood", "Scooby snack", "Hydra", "poisonous potato", "dead bush", "repair powder"
+            },
+                LicenseKey = "DFGU99-1454"
+            };*/
+
+            //filepath for save file
+            string filePath = "playerSettings.json";
+
+            //once again, test code to save the actual file
+           /* SaveManager.Instance.SaveSettings(playerSettings, filePath);*/
+
+            Player loadedSettings = SaveManager.Instance.LoadSettings(filePath);
+
+            //display the loaded settings for testing
+            Console.WriteLine($"Player Name: {loadedSettings.PlayerName}");
+            Console.WriteLine($"Level: {loadedSettings.Level}");
+            Console.WriteLine($"HP: {loadedSettings.HP}");
+            Console.WriteLine($"Inventory: {string.Join(", ", loadedSettings.Inventory)}");
+            Console.WriteLine($"License Key: {loadedSettings.LicenseKey}");
+        }
+    }
+
+    
+
+
+}
